d39fd567f281ec0a6ec46e42f1d4a49f989f51b9
姜涵 <tlos142857@live.cn>
2020-07-14 18:36:58 +0800

    度分秒转换

---
+++ HumJ.Utilities/DMS.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+namespace HumJ.Utilities
+{
+    public static class DMS
+    {
+        /// <summary>
+        /// 字符串格式的度分秒转换为浮点数格式的角度
+        /// </summary>
+        public static double DMS_Convert(this string dms)
+        {
+            var minus = Regex.IsMatch(dms, "-(?=\\d)");
+            var numbers = Regex.Matches(dms, "\\d+(\\.\\d+)?");
+
+            var d = numbers.Count > 0 ? double.Parse(numbers[0].Value) : 0;
+            var m = numbers.Count > 1 ? double.Parse(numbers[1].Value) : 0;
+            var s = numbers.Count > 2 ? double.Parse(numbers[2].Value) : 0;
+
+            var sign = minus ? -1 : 1;
+            var result = sign * (d + m / 60 + s / 3600);
+
+            return result;
+        }
+
+        /// <summary>
+        /// 浮点数格式的角度转换为字符串格式的度分秒
+        /// </summary>
+        public static string DMS_Convert(this double dms, string format = "{0}°{1}′{2}″")
+        {
+            var minus = dms < 0;
+            var abs = Math.Abs(dms);
+
+            var d = (int)abs;
+            abs = (abs - d) * 60;
+            var m = (int)abs;
+            abs = (abs - m) * 60;
+            var s = abs;
+
+            var result = string.Format(format, (minus ? "-" : "") + d, m, s);
+            return result;
+        }
+    }
+}
---
+++ NUnitTest/DMSTests.cs
@@ -0,0 +1,36 @@
+﻿using HumJ.Utilities;
+using NUnit.Framework;
+using System;
+
+namespace NUnitTest
+{
+    public class DMSTests
+    {
+        private int count;
+        private Random random;
+
+        [SetUp]
+        public void Setup()
+        {
+            count = 8192;
+
+            random = new Random();
+        }
+
+        [Test]
+        public void Convert()
+        {
+            for (var i = 0; i < count; i++)
+            {
+                var value = (random.NextDouble() - 0.5) * 180;
+                var str = value.DMS_Convert();
+                var testValue = str.DMS_Convert();
+
+                var diff = Math.Abs(testValue - value);
+                var threshold = 1.0 / 3600 / 1000000;
+
+                Assert.LessOrEqual(diff, threshold);
+            }
+        }
+    }
+}
