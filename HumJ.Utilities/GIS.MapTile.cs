c79a2c3052f80e532c88555cd58f0909a9f14bdf
HumphreyJ <tlos142857@live.cn>
2020-11-05 17:39:39 +0800

    Update GIS.MapTile.cs

--- HumJ.Utilities/GIS.MapTile.cs
+++ HumJ.Utilities/GIS.MapTile.cs
@@ -4,17 +4,23 @@ namespace HumJ.Utilities
 {
     public static partial class GIS
     {
-        public static (int x, int y) GetGisTileXY(this (double lat, double lon) point, int z)
+        public static (int tileX, int tileY, double tx, double ty) GetGisTileInfo(this (double lat, double lon) point, int z)
         {
             double lat = point.lat;
             double lon = point.lon;
 
             double lat_rad = lat * Math.PI / 180;
 
-            double x = Math.Floor((lon + 180) / 360 * Math.Pow(2, z));
-            double y = Math.Floor((1 - Math.Log(Math.Tan(lat_rad) + 1 / Math.Cos(lat_rad)) / Math.PI) * Math.Pow(2, z - 1));
+            double x = (lon + 180) / 360 * Math.Pow(2, z);
+            double y = (1 - Math.Log(Math.Tan(lat_rad) + 1 / Math.Cos(lat_rad)) / Math.PI) * Math.Pow(2, z - 1);
 
-            return ((int)x, (int)y);
+            int tileX = (int)x;
+            int tileY = (int)y;
+
+            double tx = (x - tileX) * 256;
+            double ty = (y - tileY) * 256;
+
+            return (tileX, tileY, tx, ty);
         }
     }
 }
